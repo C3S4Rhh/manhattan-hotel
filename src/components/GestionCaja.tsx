d52fe3ab9388e4ce1a23a53285c302139a3b8561
C3S4Rhh <161380383+C3S4Rhh@users.noreply.github.com>
2026-07-28 20:57:16 -0400

    Update GestionCaja.tsx

--- src/components/GestionCaja.tsx
+++ src/components/GestionCaja.tsx
@@ -263,7 +263,7 @@ export function GestionCaja({ usuario, onClose }: any) {
                 </p>
               </div>
 
-               </div>
+              
               <div >
                
               </div>
